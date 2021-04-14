9077560281921149a34e696fceadbcb9711eb6e2
unknown <prakashmehta738@gmail.com>
2021-04-14 12:57:23 +0530

    initial commit

---
+++ pactice.py
@@ -0,0 +1,13 @@
+class Employee: #class define
+    name = "Prakash"
+    designation = "Manager"
+    salesMadeThisWeek = 6
+    def hasAchievedtarget(self): #attribute called inside class using self keyword
+        if self.salesMadeThisWeek >= 5:
+            print("target has been achieved")
+        else:
+            print("Target has not been achieved")
+
+employeeOne = Employee() #object instance
+print(employeeOne.name) #invoked(called) class attribute
+print(employeeOne.hasAchievedtarget())
